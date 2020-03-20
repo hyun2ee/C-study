5e1823a29a0773a93cc9a8a0c8958380df10ee7c
hyun2ee <money04052@naver.com>
2020-03-20 23:41:53 +0900

    first version

---
+++ practice01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter03 {
+    class practice01 {
+        static void main(string[] args) {
+            byte a = 10;
+            sbyte b = -125;
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+    }
+}
